d09c953bc9954f333ce820cde78af3b5b35aaeb2
neujo <neujo@JuanToAl>
2025-11-16 12:47:43 -0500

    Carga Inicial

---
+++ src/main/java/com/mycompany/sistemagestionhotel/SistemaGestionHotel.java
@@ -0,0 +1,9 @@
+
+package com.mycompany.sistemagestionhotel;
+
+public class SistemaGestionHotel {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
